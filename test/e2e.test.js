9644c0797798d4f1e7563fe717d2185cdb199e72
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-03-22 15:18:28 +0100

    Trying to fix the open handles issue...

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -13,52 +13,82 @@ describe('metadata-service', () => {
   })
 
   it('fetches a specific movie given an ImdbID', async () => {
-    const response = await request(micro(server)).get('/?id=tt0076759')
-    expect(response.body).toMatchInlineSnapshot(`
-      Object {
-        "budget": "11000000",
-        "date_segment": "0",
-        "homepage": "http://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
-        "imdb_id": "tt0076759",
-        "original_language": "en",
-        "original_title": "Star Wars",
-        "overview": "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
-        "popularity": 42.149696999999996,
-        "poster_path": "/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg",
-        "release_date": "1977-05-25",
-        "revenue": 775398007,
-        "runtime": 121,
-        "status": "Released",
-        "tagline": "A long time ago in a galaxy far, far away...",
-        "title": "Star Wars",
-        "video": false,
-        "vote_average": 8.1,
-        "vote_count": 6778,
-      }
-    `)
+    await request(micro(server))
+      .get('/?id=tt0076759')
+      .then(response => {
+        expect(response.body).toMatchInlineSnapshot(`
+          Object {
+            "budget": "11000000",
+            "date_segment": "0",
+            "homepage": "http://www.starwars.com/films/star-wars-episode-iv-a-new-hope",
+            "imdb_id": "tt0076759",
+            "original_language": "en",
+            "original_title": "Star Wars",
+            "overview": "Princess Leia is captured and held hostage by the evil Imperial forces in their effort to take over the galactic Empire. Venturesome Luke Skywalker and dashing captain Han Solo team together with the loveable robot duo R2-D2 and C-3PO to rescue the beautiful princess and restore peace and justice in the Empire.",
+            "popularity": 42.149696999999996,
+            "poster_path": "/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg",
+            "release_date": "1977-05-25",
+            "revenue": 775398007,
+            "runtime": 121,
+            "status": "Released",
+            "tagline": "A long time ago in a galaxy far, far away...",
+            "title": "Star Wars",
+            "video": false,
+            "vote_average": 8.1,
+            "vote_count": 6778,
+          }
+        `)
+      })
   })
 
   it('fetches a random movie if no ImdbID is given', async () => {
-    const response = await request(micro(server)).get('/')
-    expect(response.body).toMatchSnapshot({
-      budget: expect.any(String),
-      date_segment: expect.any(String),
-      homepage: expect.any(String),
-      imdb_id: expect.any(String),
-      original_language: expect.any(String),
-      original_title: expect.any(String),
-      overview: expect.any(String),
-      popularity: expect.any(Number),
-      poster_path: expect.any(String),
-      release_date: expect.any(String),
-      revenue: expect.any(Number),
-      runtime: expect.any(Number),
-      status: expect.any(String),
-      tagline: expect.any(String),
-      title: expect.any(String),
-      video: expect.any(Boolean),
-      vote_average: expect.any(Number),
-      vote_count: expect.any(Number),
-    })
+    await request(micro(server))
+      .get('/')
+      .then(response => {
+        expect(response.body).toMatchInlineSnapshot(
+          {
+            budget: expect.any(String),
+            date_segment: expect.any(String),
+            homepage: expect.any(String),
+            imdb_id: expect.any(String),
+            original_language: expect.any(String),
+            original_title: expect.any(String),
+            overview: expect.any(String),
+            popularity: expect.any(Number),
+            poster_path: expect.any(String),
+            release_date: expect.any(String),
+            revenue: expect.any(Number),
+            runtime: expect.any(Number),
+            status: expect.any(String),
+            tagline: expect.any(String),
+            title: expect.any(String),
+            video: expect.any(Boolean),
+            vote_average: expect.any(Number),
+            vote_count: expect.any(Number),
+          },
+          `
+          Object {
+            "budget": Any<String>,
+            "date_segment": Any<String>,
+            "homepage": Any<String>,
+            "imdb_id": Any<String>,
+            "original_language": Any<String>,
+            "original_title": Any<String>,
+            "overview": Any<String>,
+            "popularity": Any<Number>,
+            "poster_path": Any<String>,
+            "release_date": Any<String>,
+            "revenue": Any<Number>,
+            "runtime": Any<Number>,
+            "status": Any<String>,
+            "tagline": Any<String>,
+            "title": Any<String>,
+            "video": Any<Boolean>,
+            "vote_average": Any<Number>,
+            "vote_count": Any<Number>,
+          }
+        `
+        )
+      })
   })
 })
