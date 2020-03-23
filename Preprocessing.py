e78231085fa99a78e8dbf58d8c933a335af63bae
Roland Schlaefli <rolandschlaefli@gmail.com>
2020-03-23 08:52:58 +0100

    Add database disposal and further assertions to the preprocessing script

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,19 +1,26 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Dependencies
-
-# In[1]:
-
-
+import requests
+import os
 import pandas as pd
 
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
-# # Data Import and Cleanup
+load_dotenv()
 
-# In[2]:
+DATABASE_URL = os.getenv("DATABASE_URL")
+assert DATABASE_URL is not None
 
+POSTER_SERVICE_URL = os.getenv('POSTER_SERVICE_URL')
+assert POSTER_SERVICE_URL is not None
 
+# try to fetch a random movie to ensure the poster service is working
+try:
+    random_movie = requests.get(f"{POSTER_SERVICE_URL}/")
+    assert random_movie.status_code == 200
+except:
+    raise ConnectionError()
+
+# import dataset
 movies_meta = pd.read_csv("datasets/the-movies-dataset/movies_metadata.csv")
 
 # filter out foreign movies (not en or de)
@@ -33,168 +40,77 @@ movies_meta = movies_meta.sort_values("popularity", ascending=False)
 # drop unnecessary columns
 movies_meta = movies_meta.drop(["adult", "belongs_to_collection", "id"], axis=1)
 
-movies_meta.head(2)
-
-
-# In[3]:
-
-
 # find and purge duplicated imdb ids
 movies_meta["is_duplicate"] = movies_meta.duplicated(["imdb_id"])
 movies_meta = movies_meta[~movies_meta.is_duplicate]
-max(movies_meta.imdb_id.value_counts())
-
-
-# In[4]:
-
 
 # TODO: we might actually want to use these columns, but we do not save them to the DB in a first iteration!
-movies_meta = movies_meta.drop(["genres", "production_companies", "spoken_languages", "production_countries", "is_duplicate"], axis=1)
+movies_meta = movies_meta.drop(
+    [
+        "genres",
+        "production_companies",
+        "spoken_languages",
+        "production_countries",
+        "is_duplicate",
+    ],
+    axis=1,
+)
 
-
-# # Segmentation based on Release Date
-
-# In[5]:
-
-
+# segment movies based on date buckets
 def assign_date_bucket(row):
     as_dt = pd.to_datetime(row["release_date"])
     year = as_dt.year
-    
+
     # before 1990
     if year < 1990:
         return 0
-    
+
     # 1990-2005
     elif year < 2005:
         return 1
-    
+
     # 2005-2020
     return 2
 
+
 movies_meta["date_segment"] = movies_meta.apply(assign_date_bucket, axis=1)
-movies_meta.head(2)
-
-
-# In[6]:
-
-
-# movies_meta.info()
-
-
-# In[7]:
-
-
 movies_until_1990 = movies_meta[movies_meta["date_segment"] == 0]
-movies_until_1990.head(2)
-
-
-# In[8]:
-
-
 movies_until_2005 = movies_meta[movies_meta["date_segment"] == 1]
-movies_until_2005.head(2)
-
-
-# In[9]:
-
-
 movies_until_2020 = movies_meta[movies_meta["date_segment"] == 2]
-movies_until_2020.head(2)
 
-
-# # Extract Top50
-
-# In[10]:
-
-
+# extract top50 from all segments
 top50_until_1990 = movies_until_1990.iloc[0:50]
-top50_until_1990.head(2)
-
-
-# In[11]:
-
-
 top50_until_2005 = movies_until_2005.iloc[0:50]
-top50_until_2005.head(2)
-
-
-# In[12]:
-
-
 top50_until_2020 = movies_until_2020.iloc[0:50]
-top50_until_2020.head(2)
-
-
-# # Ensure Consistency with Poster-Service
-
-# In[13]:
-
-
-import requests
-
-
-# In[14]:
-
-
 top50_all = pd.concat([top50_until_1990, top50_until_2005, top50_until_2020])
+print(top50_all.head())
 
 
-# In[15]:
-
-
-top50_all.head(2)
-
-
-# In[24]:
-
-
+# check all movies in the top50 for consistency with omdb
 def check_omdb_existence(row):
     try:
-        result = requests.get(f"http://localhost:3000/?id={row['imdb_id']}")
+        result = requests.get(f"{POSTER_SERVICE_URL}/?id={row['imdb_id']}")
         return result.status_code == 200
+
+    except ConnectionError:
+        raise ConnectionError()
+
     except:
         return False
 
+
 top50_all["omdb_consistent"] = top50_all.apply(check_omdb_existence, axis=1)
 
+try:
+    # setup a database connection
+    engine = create_engine(DATABASE_URL, echo=True)
 
-# In[25]:
+    # write the results to the database
+    top50_all = top50_all.set_index("imdb_id")
+    # top50_all[top50_all["omdb_consistent"]].to_sql(
+    #     "movies", con=engine, if_exists="replace"
+    # )
 
-
-top50_all[~top50_all["omdb_consistent"]]
-
-
-# # Hydrate Database
-
-# In[26]:
-
-
-# conda install sqlalchemy, psycopg2
-# pip install python-dotenv
-
-import os
-
-from sqlalchemy import create_engine
-from dotenv import load_dotenv
-load_dotenv()
-
-
-# In[27]:
-
-
-engine = create_engine(os.getenv("DATABASE_URL"), echo=True)
-
-
-# In[30]:
-
-
-top50_all = top50_all.set_index("imdb_id")
-top50_all[~top50_all["omdb_consistent"]].to_sql("movies", con=engine, if_exists="replace")
-
-
-# In[ ]:
-
-
-
-
+finally:
+    # get rid of the connection pool
+    engine.dispose()
